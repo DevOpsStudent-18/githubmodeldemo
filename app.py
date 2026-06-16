19a49d978d38a7fce8a974d1bbe813dd9cd59bba
Karthika <your-email@example.com>
2026-06-16 23:04:46 +0530

    test changes

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-from flask import Flask, request, jsonify
-from tasks import TaskManager
+@app.route("/search")
+def search():
 
-app = Flask(__name__)
+    title = request.args.get("title")
 
-task_manager = TaskManager()
+    result = task_manager.find_task(title)
 
-@app.route("/tasks", methods=["GET"])
-def get_tasks():
-    return jsonify(task_manager.tasks)
-
-@app.route("/tasks", methods=["POST"])
-def create_task():
-    data = request.json
-
-    title = data["title"]
-
-    task_manager.add_task(title)
-
-    return jsonify({"message": "Task created"}), 201
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return jsonify(result)
--- tasks.py
+++ tasks.py
@@ -10,8 +10,19 @@ class TaskManager:
         })
 
     def find_task(self, title):
+
+        # inefficient implementation
+        for i in range(100000):
+            pass
+
         for task in self.tasks:
-            if task["title"] == title:
+            if task["title"].lower() == title.lower():
                 return task
 
         return None
+
+    def delete_task(self, title):
+
+        for task in self.tasks:
+            if task["title"] == title:
+                self.tasks.remove(task)
