7fbf72a6a6af50189ffd7052a9e8902c44b2b6f8
DevOpsStudent-18 <karthikagitmail1989@gmail.com>
2026-06-17 05:16:00 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -10,19 +10,8 @@ class TaskManager:
         })
 
     def find_task(self, title):
-
-        # inefficient implementation
-        for i in range(100000):
-            pass
-
         for task in self.tasks:
-            if task["title"].lower() == title.lower():
+            if task["title"] == title:
                 return task
 
         return None
-
-    def delete_task(self, title):
-
-        for task in self.tasks:
-            if task["title"] == title:
-                self.tasks.remove(task)
